c6c9345a9e028d8b945101d1f1105672b87dc2d7
Sriharsha Koduri <sriharshakoduri543@gmail.com>
2017-10-13 18:18:35 +0530

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,232 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+<meta charset="utf-8">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 2632px;
+            height: 2628px;
+        }
+        .Layout-img
+        {
+            background:url("/2BHK.png");
+            background-repeat:no-repeat;
+            background-position:center;
+           background-size:80% 100%;
+           height:700px;
+        }
+        #MasterBedRoomButton
+        {
+            position:absolute;
+            align-content:center;
+            margin-left:370px;
+        }
+
+        #BedRoomButton
+        {
+            position:inherit;
+            margin-left:150px;
+        }
+
+         #BedRoomToiletButton
+        {
+            position:absolute;
+            margin-left:40px;
+        }
+
+         #MasterBedRoomToiletButton
+        {
+            position:inherit;
+            margin-left:400px;
+        }
+
+         #FamilyRoonToiletButton
+        {
+            position:absolute;
+            margin-left:40px;
+        }
+            #DinningRoomButton
+        {
+            position:inherit;
+            margin-left:400px;
+        }
+
+         #FamilyRoomButton
+        {
+            position:absolute;
+            margin-left:150px;
+        }
+
+
+          #KitchenRoomButton
+        {
+            position:inherit;
+            margin-left:400px;
+        }
+
+              #LivingRoomButton
+        {
+            position:inherit;
+            margin-left:150px;
+        }
+
+
+     </style>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+    
+    <div class="container-fluid">
+
+    <form id="form1" runat="server">
+            
+    <div class="col-sm-12">    
+        <asp:ScriptManager ID="ScriptManager1" runat="server">
+        </asp:ScriptManager>
+        
+        
+        <asp:Button ID="ServerResponseBtton" runat="server" OnClick="onclick" Text="Button" />
+    </div>  
+              
+        <div id="Response" runat="server"></div> <br />
+        <div id="LinkCheck" runat="server"></div> <br />
+        <div id="LightStatus" runat="server"></div>
+
+         <div ID="Test" runat="server">HAI I am Test Am i Helpful </div>
+
+    <div class="Layout-img row">
+        <br /><br /><br /><br /><br /><br /><br />
+
+        <div class="row col-sm-12">
+        <div class="butone col-sm-6">
+
+            <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+            <ContentTemplate>
+                <fieldset>
+                    <asp:ImageButton ID="MasterBedRoomButton" runat="server" ImageUrl="~/TL2_30X30.PNG" OnClick="bgchange" />
+                </fieldset>
+            </ContentTemplate>
+                 <Triggers>
+      <asp:AsyncPostBackTrigger ControlID="Timer1" EventName="Tick" />
+   </Triggers>
+        </asp:UpdatePanel> 
+            <asp:Timer ID="Timer1" runat="server" Interval="10000" OnTick="Timer1_Tick">
+</asp:Timer>
+       
+        </div>
+              
+        <div class="butwo col-sm-3">
+            <asp:UpdatePanel ID="UpdatePanel2" runat="server">
+            <ContentTemplate>
+                <fieldset>
+                
+                <asp:ImageButton ID="BedRoomButton" runat="server" OnClick="bgchange" ImageUrl="~/TL2_30X30.PNG" /> 
+                </fieldset>
+            </ContentTemplate>
+            </asp:UpdatePanel>                  
+        </div>   
+        
+         <div class="buthr col-sm-3">
+             <asp:UpdatePanel ID="UpdatePanel3" runat="server">
+            <ContentTemplate>
+                <fieldset>
+             
+                <asp:ImageButton ID="BedRoomToiletButton" runat="server" ImageUrl="~/TL2_30X30.PNG" OnClick="bgchange"/>
+                </fieldset>
+            </ContentTemplate>
+            </asp:UpdatePanel>            
+            </div> 
+            </div>
+
+        <br /><br /><br /><br /><br />
+
+         <div class="row col-sm-12">
+        <div class="butone col-sm-6">
+            <asp:UpdatePanel ID="UpdatePanel4" runat="server">
+            <ContentTemplate>
+                <fieldset>
+                
+                <asp:ImageButton ID="MasterBedRoomToiletButton" runat="server" ImageUrl="~/TL2_30X30.PNG" OnClick="bgchange"/>
+                </fieldset>
+            </ContentTemplate>
+            </asp:UpdatePanel>         
+        </div>
+              
+        <div class="butwo col-sm-3">
+                
+        </div>   
+        
+         <div class="buthr col-sm-3">
+              <asp:UpdatePanel ID="UpdatePanel5" runat="server">
+            <ContentTemplate>
+                <fieldset>
+                
+                <asp:ImageButton ID="FamilyRoonToiletButton" runat="server" ImageUrl="~/TL2_30X30.PNG" OnClick="bgchange"/>
+                </fieldset>
+            </ContentTemplate>
+            </asp:UpdatePanel>           
+            </div> 
+            </div>
+
+        <br /><br /><br /><br /><br /><br />
+        <div class="row col-sm-12">
+        <div class="butone col-sm-6">
+            <asp:UpdatePanel ID="UpdatePanel6" runat="server">
+            <ContentTemplate>
+                <fieldset>                
+                 <asp:ImageButton ID="DinningRoomButton" runat="server" ImageUrl="~/TL2_30X30.PNG" OnClick="bgchange"/>
+                </fieldset>
+            </ContentTemplate>
+            </asp:UpdatePanel>       
+        </div>                    
+         <div class="buthr col-sm-3">
+              <asp:UpdatePanel ID="UpdatePanel7" runat="server">
+            <ContentTemplate>
+                <fieldset>                
+                 <asp:ImageButton ID="FamilyRoomButton" runat="server" ImageUrl="~/TL2_30X30.PNG" OnClick="bgchange"/>
+                </fieldset>
+            </ContentTemplate>
+            </asp:UpdatePanel>            
+            </div> 
+            </div>
+
+           <br /><br /><br /><br /><br /><br /><br /><br />
+
+         <div class="row col-sm-12">
+        <div class="butone col-sm-6">
+            <asp:UpdatePanel ID="UpdatePanel8" runat="server">
+            <ContentTemplate>
+                <fieldset>                
+                <asp:ImageButton ID="KitchenRoomButton" runat="server" ImageUrl="~/TL2_30X30.PNG" OnClick="bgchange"/>
+                </fieldset>
+            </ContentTemplate>
+            </asp:UpdatePanel>        
+        </div>
+              
+       
+        
+         <div class="buthr col-sm-6">
+              <asp:UpdatePanel ID="UpdatePanel9" runat="server">
+            <ContentTemplate>
+                <fieldset>                
+                <asp:ImageButton ID="LivingRoomButton" runat="server" ImageUrl="~/TL2_30X30.PNG" OnClick="bgchange"/>
+                </fieldset>
+            </ContentTemplate>
+            </asp:UpdatePanel>            
+            </div> 
+            </div>
+
+
+           
+     </div>        
+    </form>         
+   </div>      
+ 
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,454 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Newtonsoft.Json;
+using System.Text;
+using System.Drawing;
+using System.Web.Configuration;
+
+public partial class _Default : System.Web.UI.Page
+{
+
+    public int MasterBedroomStatus = 0;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+       getStatus();
+    }
+
+    private void getStatus()
+    {
+        byte[] data = new byte[1024];
+        //string input, stringData;
+        IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("192.168.0.151"), 234);
+        Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+        Random Statreq = new Random();
+        int index = Statreq.Next(0, 5);
+
+
+        Statusobject stobj = new Statusobject
+        {
+            Request = "GetControlStatus",
+            RequestID = index.ToString(),
+            Controls = new Control[] { new Control() { ID = "37", StatusValue = "255" } },
+            Version = "1501603666",
+            RequestedBy = "DefaultUser"
+        };
+
+
+        string output = JsonConvert.SerializeObject(stobj);
+        data = Encoding.ASCII.GetBytes(output);
+        server.SendTo(data, data.Length, SocketFlags.None, ipep);               //Send Request to Server
+        EndPoint Remote = (EndPoint)new IPEndPoint(IPAddress.Any, 0);
+        data = new byte[1024];
+
+        int recv = server.ReceiveFrom(data, ref Remote);                        //Reseive Response from Server
+        Response.InnerHtml = Server.HtmlEncode("Message received from {0}:" + Remote);
+
+        string json = data.ToString();
+
+        ResponseObj Deserial = JsonConvert.DeserializeObject<ResponseObj>(Encoding.ASCII.GetString(data, 0, recv));
+
+        LinkCheck.InnerHtml = Server.HtmlEncode("\n This is output\t" + Deserial.Data.ControlStatus[0].StatusValue + "\n\nTIME" + DateTime.Now);
+
+        var ha = Deserial.Data.ControlStatus[0].StatusValue;
+
+        int StatuValueInt = Convert.ToInt32(ha);
+
+        if(StatuValueInt > 0)
+        {
+            MasterBedroomStatus = 1;
+            
+            MasterBedRoomButton.BackColor = Color.White;
+
+        }
+        else
+        {
+            MasterBedroomStatus = 0;
+            
+            MasterBedRoomButton.BackColor = Color.LightGray;
+        }
+        
+
+
+
+    }
+
+
+    protected void Timer1_Tick(object sender, EventArgs e)
+    {
+        Random r = new Random();
+        int index = r.Next(0, 5);
+        LinkCheck.InnerHtml = Server.HtmlEncode(index.ToString());
+        getStatus();
+
+    }
+
+
+
+    protected void onclick(object sender, EventArgs e)
+    {
+        byte[] data = new byte[1024];
+        //string input, stringData;
+
+        IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("192.168.0.151"), 234);
+        Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+        Random Statreq1 = new Random();
+        int index1 = Statreq1.Next(0, 5);
+
+
+        Requestobject obj1 = new Requestobject
+        {
+            Request = "LinkCheck",
+            RequestID = index1.ToString(),
+            Version = "1501603666"
+        };
+
+
+        string output = JsonConvert.SerializeObject(obj1);
+        Console.WriteLine(output);
+        data = Encoding.ASCII.GetBytes(output);
+        server.SendTo(data, data.Length, SocketFlags.None, ipep);
+
+        EndPoint Remote = (EndPoint)new IPEndPoint(IPAddress.Any, 0);
+        data = new byte[1024];
+
+        int recv = server.ReceiveFrom(data, ref Remote);
+        Response.InnerHtml = Server.HtmlEncode("Message received from {0}:" + Remote);
+
+        LinkCheck.InnerHtml = Server.HtmlEncode(Encoding.ASCII.GetString(data, 0, recv) + "TIME" + DateTime.Now);
+
+        Random Statreq = new Random();
+        int index = Statreq.Next(0, 5);
+
+
+        Statusobject stobj = new Statusobject
+        {
+            Request = "GetControlStatus",
+            RequestID = index.ToString(),
+            Controls = new Control[] { new Control() { ID = "37", StatusValue = "255" } },
+            Version = "1501603666",
+            RequestedBy = "DefaultUser"
+        };
+
+
+        output = JsonConvert.SerializeObject(stobj);
+        Console.WriteLine(output);
+        data = Encoding.ASCII.GetBytes(output);
+        server.SendTo(data, data.Length, SocketFlags.None, ipep);
+
+        sender = new IPEndPoint(IPAddress.Any, 0);
+        Remote = (EndPoint)(sender);
+        data = new byte[1024];
+
+        recv = server.ReceiveFrom(data, ref Remote);
+        Console.WriteLine("Message received from {0}:", Remote.ToString());
+
+        Console.WriteLine(recv);
+
+        Console.WriteLine(Encoding.ASCII.GetString(data, 0, recv));
+        LightStatus.InnerHtml = Server.HtmlEncode(Encoding.ASCII.GetString(data, 0, recv));
+
+
+
+    }
+    public class Requestobject
+    {
+        public string Request { get; set; }
+        public string RequestID { get; set; }
+        public string Version { get; set; }
+    }
+
+    public class Control
+    {
+
+        public string ID { get; set; }
+        public string StatusValue { get; set; }
+    }
+
+
+    public class Statusobject
+    {
+        public string Request { get; set; }
+        public string RequestID { get; set; }
+        public Control[] Controls { get; set; }
+        public string Version { get; set; }
+        public string RequestedBy { get; set; }
+    }
+
+
+    public class ResponseObj
+    {
+        public string ResponseFor { get; set; }
+        public int RequestID { get; set; }
+        public Data Data { get; set; }
+        public string ResponseFlag { get; set; }
+    }
+
+    public class Data
+    {
+        public Controlstatu[] ControlStatus { get; set; }
+    }
+
+    public class Controlstatu
+    {
+        public int ID { get; set; }
+        public int StatusValue { get; set; }
+        public string LastOperationTime { get; set; }
+        public int OperationType { get; set; }
+        public string OperatedBy { get; set; }
+        public int State { get; set; }
+    }
+
+    public class SwitchControl
+    {
+
+
+        public string Request { get; set; }
+        public string RequestID { get; set; }
+        public Control[] Controls { get; set; }
+        public string Version { get; set; }
+        public string RequestedBy { get; set; }
+    }
+
+
+
+
+    public static int  b2 = 0, b3 = 0, b4 = 0, b5 = 0, b6 = 0, b7 = 0, b8 = 0, b9 = 0;
+
+
+    public void bgchange(object sender, ImageClickEventArgs e)
+    {
+        ImageButton clickedButton = sender as ImageButton;
+
+        if (clickedButton.ID == "MasterBedRoomButton")
+        {
+             
+            if (MasterBedroomStatus == 0)
+            {
+                clickedButton.BackColor = Color.White;
+                SwitchOn(sender,e);
+                Test.InnerHtml = Server.HtmlEncode("Test On");
+                MasterBedroomStatus = 1;
+            }
+            else
+            {
+                clickedButton.BackColor = Color.LightGray;
+                SwitchOff(sender, e);
+                Test.InnerHtml = Server.HtmlEncode("Test Off");
+                MasterBedroomStatus = 0;
+            }
+
+        }
+
+       else if (clickedButton.ID == "BedRoomButton")
+        {
+            if (b2 == 0)
+            {
+                clickedButton.BackColor = Color.LightGray;
+                b2 = 1;
+            }
+            else
+            {
+                clickedButton.BackColor = Color.White;
+                b2 = 0;
+            }
+
+        }
+
+
+        else if (clickedButton.ID == "BedRoomToiletButton")
+        {
+            if (b3 == 0)
+            {
+                clickedButton.BackColor = Color.LightGray;
+                b3 = 1;
+            }
+            else
+            {
+                clickedButton.BackColor = Color.White;
+                b3 = 0;
+            }
+
+        }
+
+
+        else if (clickedButton.ID == "MasterBedRoomToiletButton")
+        {
+            if (b4 == 0)
+            {
+                clickedButton.BackColor = Color.LightGray;
+                b4 = 1;
+            }
+            else
+            {
+                clickedButton.BackColor = Color.White;
+                b4 = 0;
+            }
+
+        }
+
+        else if (clickedButton.ID == "FamilyRoonToiletButton")
+        {
+            if (b4 == 0)
+            {
+                clickedButton.BackColor = Color.LightGray;
+                b4 = 1;
+            }
+            else
+            {
+                clickedButton.BackColor = Color.White;
+                b4 = 0;
+            }
+
+        }
+
+        else if (clickedButton.ID == "DinningRoomButton")
+        {
+            if (b4 == 0)
+            {
+                clickedButton.BackColor = Color.LightGray;
+                b4 = 1;
+            }
+            else
+            {
+                clickedButton.BackColor = Color.White;
+                b4 = 0;
+            }
+
+        }
+        else if (clickedButton.ID == "FamilyRoomButton")
+        {
+            if (b4 == 0)
+            {
+                clickedButton.BackColor = Color.LightGray;
+                b4 = 1;
+            }
+            else
+            {
+                clickedButton.BackColor = Color.White;
+                b4 = 0;
+            }
+
+        }
+
+        else if (clickedButton.ID == "KitchenRoomButton")
+        {
+            if (b4 == 0)
+            {
+                clickedButton.BackColor = Color.LightGray;
+                b4 = 1;
+            }
+            else
+            {
+                clickedButton.BackColor = Color.White;
+                b4 = 0;
+            }
+
+        }
+        else if (clickedButton.ID == "LivingRoomButton")
+        {
+            if (b4 == 0)
+            {
+                clickedButton.BackColor = Color.LightGray;
+                b4 = 1;
+            }
+            else
+            {
+                clickedButton.BackColor = Color.White;
+                b4 = 0;
+            }
+
+        }
+
+
+
+    }
+
+    public void SwitchOn(object sender, ImageClickEventArgs e)
+    {
+        byte[] data = new byte[1024];
+        //string input, stringData;
+        IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("192.168.0.151"), 234);
+        Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+        Random Statreq = new Random();
+        int index = Statreq.Next(0, 5);
+
+
+        SwitchControl swt = new SwitchControl
+        {
+            Request = "SwitchControl",
+            RequestID = index.ToString(),
+            Controls = new Control[] { new Control() { ID = "37", StatusValue = "255" } },
+
+            Version = "1501603666",
+            RequestedBy = "DefaultUser"
+        };
+
+        string output = JsonConvert.SerializeObject(swt);
+
+         data = Encoding.ASCII.GetBytes(output);
+        Console.WriteLine(Encoding.ASCII.GetString(data));
+
+        Test.InnerHtml = "data";
+        server.SendTo(data, data.Length, SocketFlags.None, ipep);
+
+        sender = new IPEndPoint(IPAddress.Any, 0);
+        EndPoint Remote = (EndPoint)(sender);
+        data = new byte[1024];
+
+        int recv = server.ReceiveFrom(data, ref Remote);
+        Console.WriteLine("Message received from {0}:", Remote.ToString());
+
+        Console.WriteLine(recv);
+
+        Console.WriteLine(Encoding.ASCII.GetString(data, 0, recv));
+        LightStatus.InnerHtml = Server.HtmlEncode(Encoding.ASCII.GetString(data, 0, recv));
+    }
+
+
+    public void SwitchOff(object sender, ImageClickEventArgs e)
+    {
+        byte[] data = new byte[1024];
+        //string input, stringData;
+        IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("192.168.0.151"), 234);
+        Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+        Random Statreq = new Random();
+        int index = Statreq.Next(0, 5);
+
+        SwitchControl swtOff = new SwitchControl
+        {
+            Request = "SwitchControl",
+            RequestID = index.ToString(),
+            Controls = new Control[] { new Control() { ID = "37", StatusValue = "0" } },
+            Version = "1501603666",
+            RequestedBy = "DefaultUser"
+        };
+
+        string output = JsonConvert.SerializeObject(swtOff);
+
+        data = Encoding.ASCII.GetBytes(output);
+        server.SendTo(data, data.Length, SocketFlags.None, ipep);
+
+        sender = new IPEndPoint(IPAddress.Any, 0);
+        EndPoint Remote = (EndPoint)(sender);
+        data = new byte[1024];
+
+        int recv = server.ReceiveFrom(data, ref Remote);
+
+        
+        LightStatus.InnerHtml = Server.HtmlEncode(Encoding.ASCII.GetString(data, 0, recv));
+    }
+
+
+
+}
